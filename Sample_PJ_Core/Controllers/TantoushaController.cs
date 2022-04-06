9f172288e94b0eb9e4fd138238bfe0cdd11260b9
cnsa018 <comnetyamin93@gmail.com>
2022-04-06 17:24:38 +0630

    file create

---
+++ Sample_PJ_Core/Controllers/TantoushaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample_PJ_Core.Models.Context;
+using Sample_PJ_Core.Models.Editdata;
+
+namespace Sample_PJ_Core.Controllers
+{
+    public class TantoushaController : Controller
+    {
+        private readonly Context _context;
+        public TantoushaController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //_context.GetAllTantousha();
+            //return View();
+
+            Context context = HttpContext.RequestServices.GetService(typeof(Sample_PJ_Core.Models.Context.Context)) as Context;
+
+            var list_tan = context.GetAllTantousha();
+
+            return View(list_tan);
+        }
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    var cnt = new Context();
+        //    return View(cnt);
+        //}
+
+    }
+}
---
+++ Sample_PJ_Core/Models/Editdata/Tantousha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sample_PJ_Core.Models.Editdata
+{
+    [Table("m_j_tantousha")]
+    public class Tantousha
+    {
+        public string cTANTOUSHA { get; set; }
+
+        public string sTANTOUSHA { get; set; }
+
+    }
+}
